bd1028bb4897fe612b2fda9d72f1ee1741552fa7
elmar02 <babayefff02@gmail.com>
2024-12-18 21:08:15 +0400

    added basename to the route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/movie-app'>
         <Provider store={store}>
             <App />
         </Provider>
